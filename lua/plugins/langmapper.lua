d2e175c03d4c6fdf164ffeabdae36f4228e79f50
Wansmer <wansmer@gmail.com>
2023-03-22 20:50:01 +0300

    Hack which-key

--- lua/plugins/langmapper.lua
+++ lua/plugins/langmapper.lua
@@ -3,7 +3,6 @@ return {
   lazy = false,
   priority = 1, -- High priority is needed if you will use `autoremap()`
   config = function()
-    require("langmapper").setup({--[[ your config ]]
-    })
+    require("langmapper").setup()
   end,
 }
